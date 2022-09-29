8a309d030528aff74179602e0e85b97b6de913ae
JoshBennett793 <joshbennett793@yahoo.com>
2022-09-28 23:23:51 -0600

    Add homepage body rendering functionality

--- src/home.js
+++ src/home.js
@@ -1,6 +1,12 @@
-import createHeader from "./header.js";
+import createHeader from "./components/header.js";
+import createBody from "./components/home-body.js";
+import createFooter from "./components/footer.js";
 import "./css/header.css";
+import "./css/home-body.css";
+import "./css/footer.css";
 
-export default function createBody() {
+export default function render() { 
   document.body.appendChild(createHeader());
+	document.body.appendChild(createBody());
+	document.body.appendChild(createFooter());
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import createBody from './home.js';
+import render from './home.js';
 
-createBody();
+render();
 
+// Photo by <a href="https://unsplash.com/@_staticvoid?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Lucas Santos</a> on <a href="https://unsplash.com/s/photos/beer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
 
