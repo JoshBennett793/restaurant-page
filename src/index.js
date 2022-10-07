27b81dcdfcc55ac182a4f588c2e44e44011faaf8
JoshBennett793 <joshbennett793@yahoo.com>
2022-10-06 22:09:23 -0600

    Call contact page render functions

--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@ import "./index.css";
 import "./css/header.css";
 import "./css/footer.css";
 
-
-import renderHome from "./home.js";
+// import renderHome from "./home.js";
 // import renderMenu from "./menu.js"
-// // import renderContact from "./contact.js";
+import renderForm from "./contact.js";
+
 
 renderHeader();
-renderFooter();
-
-renderHome();
+// renderHome();
 // renderMenu();
-// renderContact();
+renderForm();
+renderFooter();
 
 //Photo by <a href="https://unsplash.com/@ninjason?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason Leung</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
